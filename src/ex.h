ec2e1f5e4304b3691efa529101718c90135f9132
YOSE <yosezang@163.com>
2026-07-07 15:02:51 +0200

    完成了执行命令行功能. 正在编写异常功能.

--- src/ex.h
+++ src/ex.h
@@ -4,33 +4,43 @@
 ///TODO
 #ifndef EX_H
 #define EX_H
-
+#include <print>
+using std::string;
+using std::print;
 
 class ExBs
 {
 public:
     string rsn{};
+    string arg{};
+    string arg2{};
+    string arg3{};
+    int argCnt{0};
+
+
 };
 
 class ExSntx:public ExBs
 {
-
+    ExSntx(string rsn, string arg, string arg2, string arg3): ExBs(rsn, arg, arg2, arg3){};
 };
 
 
 class ExLgc:public ExBs
 {
-
+public:
+    ExLgc(string rsn, string arg, string arg2, string arg3): ExBs(rsn, arg, arg2, arg3){};
 };
 
 class ExVrbNotFnd:public ExLgc
 {
-
+public:
+    ExVrbNotFnd(string vrb): ExLgc("Variable {} not found.\n", vrb, string{}, string{}){};
 };
 
 class ExVrbRdfn : public ExLgc
 {
-
+    ExVrbRdfn(string vrb): ExLgc("Variable {} cannot be redefined.\n", vrb, string{}, string{}){};
 };
 
 class ExWrgOprndTyp:public ExLgc
@@ -39,6 +49,13 @@ class ExWrgOprndTyp:public ExLgc
 };
 
 class ExFcnNotFnd: public ExLgc
+{
+
+    ExFcnNotFnd(string fn): ExLgc("Function {} not found.\n", fn, string{}, string{}){};
+
+};
+
+class ExFcnRdfn: public ExLgc
 {
 
 };
@@ -53,10 +70,7 @@ class ExFcnTooFewArg: public ExLgc
 
 };
 
-class ExFcnRdfn: public ExLgc
-{
 
-};
 
 class ExAlrdDfnAsVrb: public ExLgc
 {
